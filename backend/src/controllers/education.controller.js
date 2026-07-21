90c2991e29f2292208f27506552f3853284d018f
Norbert <norbertzaleskiii@gmail.com>
2026-07-21 22:51:03 +0200

    u

---
+++ backend/src/controllers/education.controller.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+export const getEducation = async(req,res) => {
+    res.status(200).send("yo edu")
+};
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express"
 import accountRoutes from "./account.routes.js"
 import budgetRoutes from "./budget.routes.js";
+import educationRoutes from "./education.routes.js";
 
 const router = express.Router();
 
 router.use("/account", accountRoutes);
 router.use("/budget", budgetRoutes);
+router.use("/education", educationRoutes);
 
 export default router;
